e0667fbe3d56977db0fb08da3c2b8976a8e7e2a9
Fabian Gröger <fabian.groeger@icloud.com>
2023-03-09 18:31:35 +0100

    Initial Commit

---
+++ RandomJokeApp/RandomJokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomJokeAppApp.swift
+//  RandomJokeApp
+//
+//  Created by Fabian Gröger on 09.03.23.
+//
+
+import SwiftUI
+
+@main
+struct RandomJokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
